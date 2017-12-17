78f9e6f9ee319c094fbdbe3e94d5d978b55d67c4
stkdiretto <stefan@shutterworks.org>
2017-12-17 12:03:31 +0100

    header clock should work now

--- clock.py
+++ clock.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
-import socket, datetime
+import socket, datetime, pytz
 
-now = datetime.datetime.now()
+met = pytz.timezone("Europe/Berlin")
 
-since_midnight = (
-    now - 
-    now.replace(hour=0, minute=0, second=0)
-).seconds
+now = datetime.datetime.now(pytz.utc).astimezone(met)
+since_midnight = (now - now.replace(hour=0, minute=0, second=0)).seconds
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.sendto('node/header/clock/midnight:%f' % since_midnight, ('127.0.0.1', 4444))
 sock.sendto('node/schedule/clock/set:%d' % since_midnight, ('127.0.0.1', 4444))
+
--- node/header/node.lua
+++ node/header/node.lua
@@ -1,8 +1,38 @@
 gl.setup(1920, 200)
 
 logo = resource.load_image("jh-ulm.png")
+font = resource.load_font("OpenSans-Regular.ttf")
+
+
+local clock = (function()
+  local base_time = N.base_time or 0
+
+  local function set(time)
+    base_time = tonumber(time) - sys.now()
+  end
+
+  util.data_mapper{
+    ["clock/midnight"] = function(since_midnight)
+        set(since_midnight)
+    end;
+  }
+
+  local left = 0
+
+  local function get()
+    local time = (base_time + sys.now()) % 86400
+    return string.format("%d:%02d", math.floor(time / 3600), math.floor(time % 3600 / 60))
+  end
+
+  return {
+    get = get;
+    set = set;
+  }
+end)()
+
 
 function node.render()
   --gl.clear(1, 1, 1, 0.25)
   logo:draw(40, 30, 546, 190)
+  font:write(1400, 30, clock.get(), 160, 1,1,1,1)
 end
